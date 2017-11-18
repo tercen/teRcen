5a908e7757fdff17a75ad7f6bc8a5e8127114dba
amaurel <alexandre.maurel@gmail.com>
2017-11-18 08:16:39 +0100

    ++

--- R/double_color_element.R
+++ R/double_color_element.R
@@ -3,8 +3,8 @@
 #' @export
 #' @format \code{\link{R6Class}} object, super class \code{\link{ColorElement}}.
 #' @field color of type int inherited from super class \code{\link{ColorElement}}.
-#' @field doubleValue of type double.
-DoubleColorElement <- R6::R6Class("DoubleColorElement", inherit = ColorElement, public = list(doubleValue = NULL, 
+#' @field stringValue of type String.
+DoubleColorElement <- R6::R6Class("DoubleColorElement", inherit = ColorElement, public = list(stringValue = NULL, 
     initialize = function(json = NULL) {
         if (!is.null(json)) {
             self$initJson(json)
@@ -13,14 +13,14 @@ DoubleColorElement <- R6::R6Class("DoubleColorElement", inherit = ColorElement, 
         }
     }, init = function() {
         super$init()
-        self$doubleValue = 0
+        self$stringValue = ""
     }, initJson = function(json) {
         super$initJson(json)
-        self$doubleValue = as.double(json$doubleValue)
+        self$stringValue = json$stringValue
     }, toTson = function() {
         m = super$toTson()
         m$kind = rtson::tson.scalar("DoubleColorElement")
-        m$doubleValue = rtson::tson.scalar(self$doubleValue)
+        m$stringValue = rtson::tson.scalar(self$stringValue)
         return(m)
     }, print = function(...) {
         cat(yaml::as.yaml(self$toTson()))
