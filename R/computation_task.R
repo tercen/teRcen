744972887b8b0265d5bac6ef1d2452bdb1cb3796
amaurel <alexandre.maurel@gmail.com>
2019-02-04 14:37:13 +0100

    parentTaskId added

--- R/computation_task.R
+++ R/computation_task.R
@@ -12,6 +12,7 @@
 #' @field isDeleted of type bool inherited from super class \code{\link{PersistentObject}}.
 #' @field rev of type String inherited from super class \code{\link{PersistentObject}}.
 #' @field id of type String inherited from super class \code{\link{IdObject}}.
+#' @field parentTaskId of type String.
 #' @field fileResultId of type String.
 #' @field query object of class \code{\link{CubeQuery}} inherited from super class \code{\link{CubeQueryTask}}.
 #' @field state object of class \code{\link{State}} inherited from super class \code{\link{Task}}.
@@ -21,8 +22,8 @@
 #' @field completedDate object of class \code{\link{Date}} inherited from super class \code{\link{Task}}.
 #' @field aclContext object of class \code{\link{AclContext}} inherited from super class \code{\link{Task}}.
 #' @field computedRelation object of class \code{\link{Relation}}.
-ComputationTask <- R6::R6Class("ComputationTask", inherit = CubeQueryTask, public = list(fileResultId = NULL, 
-    computedRelation = NULL, initialize = function(json = NULL) {
+ComputationTask <- R6::R6Class("ComputationTask", inherit = CubeQueryTask, public = list(parentTaskId = NULL, 
+    fileResultId = NULL, computedRelation = NULL, initialize = function(json = NULL) {
         if (!is.null(json)) {
             self$initJson(json)
         } else {
@@ -30,15 +31,18 @@ ComputationTask <- R6::R6Class("ComputationTask", inherit = CubeQueryTask, publi
         }
     }, init = function() {
         super$init()
+        self$parentTaskId = ""
         self$fileResultId = ""
         self$computedRelation = Relation$new()
     }, initJson = function(json) {
         super$initJson(json)
+        self$parentTaskId = json$parentTaskId
         self$fileResultId = json$fileResultId
         self$computedRelation = createObjectFromJson(json$computedRelation)
     }, toTson = function() {
         m = super$toTson()
         m$kind = rtson::tson.scalar("ComputationTask")
+        m$parentTaskId = rtson::tson.scalar(self$parentTaskId)
         m$fileResultId = rtson::tson.scalar(self$fileResultId)
         if (!is.null(self$computedRelation)) m$computedRelation = self$computedRelation$toTson()
         return(m)
--- README.md
+++ README.md
@@ -6,5 +6,5 @@ First install [rtson](https://github.com/tercen/TSON/tree/master/rtson).
 
 ```R
 install.packages('devtools')
-devtools::install_github("tercen/teRcen", ref = "0.7.1", upgrade_dependencies = FALSE, args="--no-multiarch")
+devtools::install_github("tercen/teRcen", ref = "0.8.1", upgrade_dependencies = FALSE, args="--no-multiarch")
 ```
