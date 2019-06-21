90678778cb959c2abe9433cbf225aad26d37252d
amaurel <alexandre.maurel@gmail.com>
2019-06-21 08:03:44 +0200

    improve error message

--- R/tercen-wrapper.R
+++ R/tercen-wrapper.R
@@ -324,9 +324,8 @@ OperatorContext <- R6Class(
     },
     
     save = function(computed.df){
-      print("save")
+      
       result = OperatorResult$new()
-      print("OperatorResult")
       
       if (inherits(computed.df, 'list')){
         result$tables = lapply(computed.df, tercen::dataframe.as.table)
@@ -334,11 +333,7 @@ OperatorContext <- R6Class(
         result$tables = list(tercen::dataframe.as.table(computed.df))
       }
       
-      print("result")
-      
       bytes = toTSON(result$toTson())
-      
-      print("bytes")
       
       if (nchar(self$task$fileResultId) == 0){
         # webapp scenario
@@ -363,19 +358,15 @@ OperatorContext <- R6Class(
         }
         
       } else {
-        print("fileDoc")
         fileDoc = self$client$fileService$get(self$task$fileResultId)
-        print("fileService$get")
-        
         self$client$fileService$upload(fileDoc, bytes)
-        print("fileService$upload")
       }
     }
   ),
   active = list(
     workflow = function(value){
       if (!missing(value)) stop('read only')
-      stop('not inpl')
+      stop('not impl')
       #       return (self$client$workflowService$get(self$workflowId))
     },
     query = function(value) {
@@ -396,13 +387,19 @@ tercenCtx <- function(workflowId=getOption("tercen.workflowId"),
                       username = getOption("tercen.username"), password = getOption("tercen.password"),
                       serviceUri = getOption("tercen.serviceUri", default = "https://tercen.com/service")){
   
+  
+  
   if (is.null(workflowId)){
+    if (is.null(taskId) && is.null(parseCommandArgs()$taskId)) {
+      stop("Failed to build tercen context, workflowId and stepId are required")
+    } 
     return (OperatorContext$new(taskId=taskId,
-                                authToken=authToken,
+                                authToken=authToken, 
                                 username=username, 
                                 password=password, 
                                 serviceUri=serviceUri))
   } else {
+    if (is.null(stepId)) stop("Failed to build tercen context, stepId is required")
     return (OperatorContextDev$new(workflowId=workflowId,
                                    stepId=stepId,
                                    taskId=taskId,
