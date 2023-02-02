01eeaf7f1e7bb62ff3731ff5b514e2af5d40aa05
agouy <contact@agouy.com>
2023-02-02 12:04:36 +0000

    test2

--- examples/build-and-upload.R
+++ examples/build-and-upload.R
@@ -1,12 +1,18 @@
 ### Get tag and increment package version
 ref_name <- Sys.getenv("GITHUB_REF_NAME")
-usethis:::use_description_field(
-  name = "Version",
-  value = ref_name,
-  overwrite = TRUE,
-  base_path = "."
-)
+# usethis:::use_description_field(
+#   name = "Version",
+#   value = ref_name,
+#   overwrite = TRUE,
+#   base_path = "."
+# )
 
+message(getwd())
+message("\n\n\n")
+
+message(paste0(list.files(".."), collapse="\n"))
+message("\n\n\n")
+message(paste0(list.files(".."), collapse="\n"))
 ### Build package
 pkg_path <- pkgbuild::build(path = ".", dest_path = ".")
 message(pkg_path)
