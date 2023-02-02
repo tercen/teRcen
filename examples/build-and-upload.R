8332d733a26d4a270753b72b6b2808f0b4d20bd6
agouy <contact@agouy.com>
2023-02-02 13:33:36 +0000

    removed usethis dependency

--- examples/build-and-upload.R
+++ examples/build-and-upload.R
@@ -1,10 +1,15 @@
 ### Get tag and increment package version
-ref_name <- Sys.getenv("GITHUB_REF_NAME")
-usethis:::use_description_field(
-  name = "Version",
-  value = ref_name,
-  overwrite = TRUE
-)
+ref_name <- Sys.getenv("GITHUB_REF_NAME")t
+
+semver_regex <- "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"
+if(!grepl(pattern = semver_regex, ref_name)) {
+  stop("Invalid semantic version.")
+}
+
+desc <- readLines("./DESCRIPTION")
+desc2 <- gsub(pattern = "Version:.*", replace = paste0("Version: ", ref_name), x = desc)
+desc3 <- gsub(pattern = "Date:.*", replace = paste0("Date: ", Sys.Date()), x = desc2)
+writeLines(desc3, con = "./DESCRIPTION")
 
 ### Build package
 pkg_path <- pkgbuild::build(path = ".", dest_path = ".")
