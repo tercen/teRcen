367fad01bcc3ab1788b44cbf31de9b7dbd9c6762
agouy <contact@agouy.com>
2023-11-01 12:19:44 +0000

    Added base64enc dependency

--- R/tercen.R
+++ R/tercen.R
@@ -5,5 +5,5 @@
 #' @name tercen-package
 #' @aliases tercen
 #' @docType package
-#' @import R6 tercenApi teRcenHttp mtercen dplyr tibble openssl uuid
+#' @import R6 tercenApi teRcenHttp mtercen dplyr tibble openssl uuid base64enc
 NULL
